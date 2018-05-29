67fc6ddd1f2e309532a72e1d628b5ca1232a4f49
liangzhenjie <liang.1987@qq.com>
2018-05-29 11:37:56 +0800

    update version

---
+++ serialPort_Ethernet2CAN_sdk_v3.0.0/example/CMakeFiles/homingActuator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/homingActuator.dir/src/homingActuator.cpp.o"
+  "bin/homingActuator.pdb"
+  "bin/homingActuator"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/homingActuator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
